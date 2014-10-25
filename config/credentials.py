a1b11cc0a1b1e2f6f05251486980c41398eb560e
plablo <plablo09@gmail.com>
2014-10-25 10:13:55 -0500

    Initial structure

---
+++ config/credentials.py
@@ -0,0 +1,16 @@
+
+
+def get_auth_credentials():
+    tw_consumer_key = ""
+    tw_consumer_secret = ""
+    tw_access_key = ""
+    tw_access_secret = ""
+    google_uname = ""
+    google_pwd = ""
+    credentials_dict = {"tw_consumer_key":tw_consumer_key,
+                        "tw_consumer_secret":tw_consumer_secret,
+                        "tw_access_key":tw_access_key,
+                        "tw_access_secret":tw_access_secret,
+                        "google_uname":google_uname,
+                        "google_pwd":google_pwd}
+    return credentials_dict
