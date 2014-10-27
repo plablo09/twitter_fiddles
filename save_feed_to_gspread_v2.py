6af88727a313d57b54595082d941d4c6900e73d7
plablo09 <pablo.lopez@centrogeo.edu.mx>
2014-10-27 14:59:08 -0600

    Otro intento de recuperarse de errores

--- save_feed_to_gspread_v2.py
+++ save_feed_to_gspread_v2.py
@@ -14,19 +14,17 @@ api = tweepy.API(auth)
 
 #create an object called 'customStreamListener'
 class CustomStreamListener(tweepy.StreamListener):
-    """Esta versión recibe una palabra clave y filtra el stream.
+    """En esta versión no recibimos palabras clave.
 
-    Para actualizar el renglón del google spread en el que se escribe, lleva la
-    cuenta en row_number. El problema es que cuando el script se reinicia se
-    pierde la cuenta
+    Para darle la vuelta al problema de la actualización de las filas, creamos
+    un nuevo worksheet vacio y usamos append_row() siempre
     """
-
-    def __init__(self, credentials, keyword,api=None, ):
+    def __init__(self, credentials,worksheet, api=None ):
         super(CustomStreamListener,self).__init__()
-        self.row_number = 448
-        self.keyword = keyword
-        gc = gspread.login(auth_dict['google_uname'], auth_dict['google_pwd'])
-        self.wks = gc.open('DilmaNovamente_2').sheet1
+        #self.row_number = 448
+        self.wks = worksheet
+        #gc = gspread.login(auth_dict['google_uname'], auth_dict['google_pwd'])
+        #self.wks = gc.open('DilmaNovamente_2').sheet1
 
     def write_row(self,row, row_number):
         self.wks.update_cell(row_number, 1, row[0])
@@ -43,16 +41,15 @@ class CustomStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
 
-        if self.keyword.lower() in status.text.lower():
-            if status.coordinates is not None:
-                print status.coordinates
-                print status.author.screen_name, status.created_at, status.text
-                coordinates = self.coordinates_formater(status.coordinates)
-                print coordinates
-                row = (status.author.screen_name,status.created_at,status.text,
-                        coordinates[0], coordinates[1])
-                self.write_row(row, self.row_number)
-                self.row_number += 1 #move to the next row
+
+        if status.coordinates is not None:
+            print status.coordinates
+            print status.author.screen_name, status.created_at, status.text
+            coordinates = self.coordinates_formater(status.coordinates)
+            print coordinates
+            row = (status.author.screen_name,status.created_at,status.text,
+                    coordinates[0], coordinates[1])
+            self.wks.append_row(row)
 
 
     def on_error(self, status_code):
@@ -64,15 +61,26 @@ class CustomStreamListener(tweepy.StreamListener):
         return True # Don't kill the stream
 
 
-def start_stream():
+def start_stream(worksheet):
     while True:
         try:
-            streamingAPI = tweepy.streaming.Stream(auth, CustomStreamListener(auth_dict,'DilmaNovamente'))
-            streamingAPI.filter(locations=[-180,-90,180,90])
+            streamingAPI = tweepy.streaming.Stream(auth, CustomStreamListener(auth_dict,worksheet))
+            streamingAPI.filter(locations=[-99.5,18.9,-98.5,20])
         except:
             continue
 
-start_stream()
+gc = gspread.login(auth_dict['google_uname'], auth_dict['google_pwd'])
+try:
+    sh = gc.open('store_stream')
+except HTTPError as e:
+    print(e)
+#tenemos que ver si ya existe el wks de trabajo, para no escribir miles
+if len(sh.worksheets()) >1:
+    wks = sh.get_worksheet(1)
+else:
+    wks = sh.add_worksheet(title="stream",rows="1",cols="10")
+
+start_stream(wks)
 
 # streamingAPI = tweepy.streaming.Stream(auth, CustomStreamListener(auth_dict,'DilmaNovamente'))
 # #streamingAPI.filter(track=['LaVerdaderaPruebaDeAmorConsisteEn'])-180,-90,180,90
