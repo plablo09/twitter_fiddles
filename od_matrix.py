e4b3c691af988efb911ba0420377984d57911dcd
plablo <plablo09@gmail.com>
2014-12-02 19:53:19 -0600

    Crear matriz OD y guardarla como csv

--- od_matrix.py
+++ od_matrix.py
@@ -1,41 +1,55 @@
 # -*- coding: utf-8 -*-
 import psycopg2 as psy
 from config.credentials import get_auth_credentials
+import numpy as np
 
 conn_params = get_auth_credentials()
-conn_str =" host='localhost' dbname='twitter_feed'" + "user=" + \
+conn_str =" host='localhost' dbname='tuiter'" + "user=" + \
             conn_params["dbuser"] + " password=" + conn_params["dbpassword"]
 
 conn = psy.connect(conn_str)
 
 if conn:
-    print 'me conecté'
-    q_distritos = "select cve_dist from distritos_eod limit 10;"
+    q_distritos = "select cve_dist from distritos_eod;"
     cur = conn.cursor()
     cur.execute(q_distritos)
     distritos = cur.fetchall()
-
+    distritos = sorted([d[0] for d in distritos])
+    print distritos
+    od = {}
+    matriz = np.zeros(shape=(len(distritos),len(distritos)),dtype=np.int)
     for d in distritos:
+        viajes = np.zeros(len(distritos),dtype=np.int)
         ini_fin = []
         ini_q = "select o.id, o.inicio, o.final, d.geom, d.cve_dist from \
                 o_d_morning o join distritos_eod d on \
                 st_intersects(o.inicio,d.geom) where d.cve_dist =%s"
-        cur.execute(ini_q,[d[0]])
+        cur.execute(ini_q,[d])
         inicios = cur.fetchall()
-        print len(inicios)
+        inicios = [i[0] for i in inicios]
+        #print len(inicios)
         for i in inicios:
-            print "el inicioo " + str(i[0])
+            #print "el inicioo " + str(i[0])
             fin_q = "select d.cve_dist from \
                     o_d_morning o join distritos_eod d on \
                     st_intersects(o.final,d.geom) where o.id = %s"
-            cur.execute(fin_q,[i[0]])
-            finales = cur.fetchall()
-            print "el fin  " + str(finales[0])
-            ini_fin.append((i[4],len(finales)))
+            cur.execute(fin_q,[i])
+            final = cur.fetchall()
+            if len(final):
+                idx = distritos.index(final[0][0])
+                viajes[idx] +=1
 
-        print ini_fin
+        matriz[n] = viajes
+
+
+
+    print matriz
+    np.savetxt('output/od_matrix.csv',matriz,fmt='%i',delimiter=',')
 
         #print inicios
         # for i in inicios:
         #
         #     print cur.fetchall()
+else:
+    print 'No se pudo establecer una conección con la base de datos\
+            chaca los parámetros'
