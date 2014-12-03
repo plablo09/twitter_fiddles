cfe91efdd53c1e6019b62713bf5f7e4131ce2dd5
plablo09 <pablo.lopez@centrogeo.edu.mx>
2014-12-03 12:05:51 -0600

    Resolví un bug

--- od_matrix.py
+++ od_matrix.py
@@ -4,13 +4,13 @@ from config.credentials import get_auth_credentials
 import numpy as np
 
 conn_params = get_auth_credentials()
-conn_str =" host='localhost' dbname='tuiter'" + "user=" + \
+conn_str =" host='localhost' dbname='twitter_feed'" + "user=" + \
             conn_params["dbuser"] + " password=" + conn_params["dbpassword"]
 
 conn = psy.connect(conn_str)
 
 if conn:
-    q_distritos = "select cve_dist from distritos_eod;"
+    q_distritos = "select cve_dist from distritos_eod_corrected;"
     cur = conn.cursor()
     cur.execute(q_distritos)
     distritos = cur.fetchall()
@@ -18,12 +18,12 @@ if conn:
     print distritos
     od = {}
     matriz = np.zeros(shape=(len(distritos),len(distritos)),dtype=np.int)
-    for d in distritos:
+    for n,d in enumerate(distritos):
         viajes = np.zeros(len(distritos),dtype=np.int)
         ini_fin = []
         ini_q = "select o.id, o.inicio, o.final, d.geom, d.cve_dist from \
-                o_d_morning o join distritos_eod d on \
-                st_intersects(o.inicio,d.geom) where d.cve_dist =%s"
+                o_d_afternoon o join distritos_eod_corrected d on \
+                st_intersects(o.inicio,d.geom) where d.cve_dist = %s"
         cur.execute(ini_q,[d])
         inicios = cur.fetchall()
         inicios = [i[0] for i in inicios]
@@ -31,7 +31,7 @@ if conn:
         for i in inicios:
             #print "el inicioo " + str(i[0])
             fin_q = "select d.cve_dist from \
-                    o_d_morning o join distritos_eod d on \
+                    o_d_afternoon o join distritos_eod_corrected d on \
                     st_intersects(o.final,d.geom) where o.id = %s"
             cur.execute(fin_q,[i])
             final = cur.fetchall()
@@ -44,7 +44,7 @@ if conn:
 
 
     print matriz
-    np.savetxt('output/od_matrix.csv',matriz,fmt='%i',delimiter=',')
+    np.savetxt('output/od_matrix_afternoon.csv',matriz,fmt='%i',delimiter=',')
 
         #print inicios
         # for i in inicios:
@@ -52,4 +52,4 @@ if conn:
         #     print cur.fetchall()
 else:
     print 'No se pudo establecer una conección con la base de datos\
-            chaca los parámetros'
+            checa los parámetros'
