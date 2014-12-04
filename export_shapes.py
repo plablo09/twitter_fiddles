c70b9682219029e97d16208f8978674de0f48e7e
plablo09 <pablo.lopez@centrogeo.edu.mx>
2014-12-04 13:34:09 -0600

    Otras formas de seleccionar lo que se quiere exportar

--- export_shapes.py
+++ export_shapes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-lista_horas = ['%s:00:00' % h for h in xrange(16,23)]
+lista_horas = ['%s:00:00' % h for h in xrange(06,23)]
 
 # "2014-10-30"
 # "2014-10-31"
@@ -20,18 +20,19 @@ lista_horas = ['%s:00:00' % h for h in xrange(16,23)]
 # "2014-11-13"
 
 
-lista_fechas =('2014-10-30','2014-10-31','2014-11-03','2014-11-04','2014-11-05','2014-11-06',
-            '2014-11-07','2014-11-10','2014-11-11','2014-11-12','2014-11-13')
+# lista_fechas =('2014-10-30','2014-10-31','2014-11-03','2014-11-04','2014-11-05','2014-11-06',
+#             '2014-11-07','2014-11-10','2014-11-11','2014-11-12','2014-11-13')
 #lista_fechas = ('2014-10-31','2014-11-01','2014-11-02','2014-11-03','2014-11-04')
+lista_fechas = ('2014-11-04',)
 
 lista_comandos = []
 for k, fecha in enumerate(lista_fechas):
     for i,h in enumerate(lista_horas):
         if i < len(lista_horas) -1:
-            sql = "select c.id, c.uname, c.text, to_char(c.hora,'HH24:MI:SS') as hora, c.geom from tweets_rectificado c where c.fecha = '" + fecha +\
+            sql = "select c.id, c.uname, c.text, to_char(c.hora,'HH24:MI:SS') as hora, st_transform(c.geom,4326) from tweets_rectificado c where c.fecha = '" + fecha +\
                     "' and (c.hora >= '" + h + "' and c.hora <= '" + \
                     lista_horas[i+1]+ "')"
-            nombre_shape = "/home/plablo/twitter_afternoons/d_"+str(k)+ "_t_"  + str(i) +".shp"
+            nombre_shape = "/home/plablo/twitter_tmp/"+"t_"  + str(i) +".shp"
             cmd = ["ogr2ogr","-f", "ESRI Shapefile",nombre_shape,
                     "PG:dbname=twitter_feed host=localhost port=5432 user=postgres password=l4szl0.l0szl4",
                     "-sql", sql,
